befa869e07f838abec972500d29dfd33a9f7bd60
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-07 21:27:28 +0700

    Add Example_003

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿//Console.Write ("Введите число A ");
+//String numberA = Console.ReadLine ();
+//Console.Write ("Введите число B ");
+//String numberB = Console.ReadLine ();
+//Console.Write ("A+B= ");
+//Console.WriteLine (numberA + numberB);
+
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
