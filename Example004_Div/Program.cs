9f7113e9f7490d6850a595f50b6213b9750ddd22
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-07 21:42:19 +0700

    Add Example004

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,11 @@
+﻿//Console.Write ("Введите число A ");
+//String numberA = Console.ReadLine ();
+//Console.Write ("Введите число B ");
+//String numberB = Console.ReadLine ();
+//Console.Write ("A+B= ");
+//Console.WriteLine (numberA + numberB);
+
+double numberA = 12;
+double numberB = 5;
+double result = numberA / numberB;
+Console.WriteLine(result);
