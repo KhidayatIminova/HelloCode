370c87d0edd91d2647d8e039aa8684ab91bb2899
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-10 14:52:37 +0700

    add HWTask2

---
+++ HomeworkTask2/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+// Условие 1
+
+int a = 8, b = 7;
+
+int max1 = a;
+
+if (a > max1) max1 = a;
+if (b > max1) max1 = b;
+
+Console.Write ("max1 = ");
+Console.WriteLine (max1);
+
+// Условие 2
+
+int c = 2, d = 10;
+
+int max2 = c;
+
+Console.Write ("max2 = ");
+
+if (d > max2) max2 = d;  
+
+{
+    Console.WriteLine (max2);
+}
+
+// Условие 3
+
+int e = -9, f = -3;   
+
+Console.Write ("max3 = ");
+
+if (f > e)  
+    
+{
+    Console.WriteLine (f);
+}
+
+else
+
+{
+    Console.WriteLine (e);    
+}
