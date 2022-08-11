0d8b54921dc3be9c0598d009e5fdf3dd4e2d2287
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-11 16:52:42 +0700

    add Sem1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear ();
+Console.Write ("Введите число 1:");
+int number1 = int.Parse(Console.ReadLine ()); //по умолчанию считывает с консоли как string, преобразовываем в int
+Console.Write ("Введите число 2:");
+int number2 = int.Parse(Console.ReadLine ());
+
+if (number2 == number1 * number1)
+{
+    Console.WriteLine($"Квадрат числа {number1} равен числу {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} не является квадратом числа {number1}");
+}
