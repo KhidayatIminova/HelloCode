c14c0c72f19aa9af918e7e04709730a3cf6634ab
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-12 14:00:21 +0700

    Add HW1_Task2

---
+++ Seminar1/Homework_1/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Семинар 1. Задача 2
+// Написать программу, которая принимает на вход 2 числа и выдает, какое большее, а какое меньшее 
+
+Console.Clear ();
+
+Console.Write ("Введите число a: ");
+int a = int.Parse(Console.ReadLine ()); 
+Console.Write ("Введите число b :");
+int b = int.Parse(Console.ReadLine ());
+
+if (a > b)  
+{
+    Console.WriteLine ($"\na = {a}; b = {b}  ->  max = {a}   (a > b)");
+}
+
+else
+    if (a < b)
+    {
+    Console.WriteLine ($"\na = {a}; b = {b}  ->  max = {b}   (a < b)");
+    }
+
+else
+{
+    Console.WriteLine ("\nЧисла a и b равны   (a = b)");
+}
