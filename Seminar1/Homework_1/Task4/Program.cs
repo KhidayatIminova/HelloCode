e83e40e316343e44c5d5d4b19e1681f25a31903d
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-12 14:32:24 +0700

    Add task4

---
+++ Seminar1/Homework_1/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Семинар 1. Задача 4
+// Написать программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел 
+
+Console.Clear ();
+
+Console.Write ("Введите число a: ");
+int a = int.Parse(Console.ReadLine ()); 
+Console.Write ("Введите число b :");
+int b = int.Parse(Console.ReadLine ());
+Console.Write ("Введите число c :");
+int c = int.Parse(Console.ReadLine ());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine ($"\na = {a}; b = {b}; c = {c}  Максимальное значение ->  max = {max}");
