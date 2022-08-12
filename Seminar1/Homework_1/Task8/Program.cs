3771b98b7901b24ae6b26c33bcf5c1f9d9fa9bca
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-12 15:59:15 +0700

    Add HW1_Task8

---
+++ Seminar1/Homework_1/Task8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Семинар 1. Задача 8
+// Написать программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear ();
+
+Console.Write ("Введите число (N > 1): ");
+int N = int.Parse(Console.ReadLine ()); 
+
+if (N <= 1)
+{
+    Console.WriteLine ($"\nЧисло {N}  находится вне допустимого диапазона");  
+}
+else
+{
+    int count = 2;
+    Console.Write ($"\nЧётные числа от 1 до {N}:");
+
+    while (count < N)
+    {
+        Console.Write ($" {count} "); 
+        count += 2;
+    }
+}
+Console.WriteLine ("\n ");
