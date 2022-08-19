80ffecc29fb839fa3c4a367b3708c85335d1e0eb
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-20 03:41:53 +0700

    Add HW4_task025

---
+++ Seminar4/Homework4/Task025/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+// Задача 25. Написать  цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+// 1. Цикл For
+
+double c = a;
+
+if (b != 0)
+
+{
+    for (int i = 1; i < Math.Abs(b); i++)
+    {
+        if (b > 0)
+        {
+            c = c * a; // целая положительная степень (натуральная)
+        }
+        else 
+        {
+            c = c / a; // целая отрицательная степень
+        }
+    }
+}
+
+else c = 1; // нулевая степень
+
+
+// 2. Без цикла
+
+// double c = Convert.ToDouble(Math.Pow(a, b));
+
+Console.WriteLine($"({a})^({b}) = {c} ");
