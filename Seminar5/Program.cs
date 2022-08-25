5ffc7b2d2cb8c2d09777c83abcee32e160855743
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-25 17:37:59 +0700

    Add Seminar5

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -75,13 +75,132 @@ Console. Clear();
 
 // Задача 31. Задать массив из 12 элементов, заполненный случайными числами из промежутка  [-9. +9]. Найти сумму отрицательных и сумму положительных элементов массива.
 
+// int [] array = GetArray(12, -9, +9);
+
+// Console.WriteLine(String.Join(" ,", array));
+// Console.WriteLine($"Сумма положительных чисел {GetPositiveSumm(array)}");
+// Console.WriteLine($"Сумма отрицательных чисел {GetNegativeSumm(array)}");
+
+// // 1. Метод, который возвращает массив, заполненный случайными числами из заданного промежутка
+
+// int [] GetArray(int count, int minValue, int maxValue)
+// {
+//     int [] array = new int [count];
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// // 2. Получить сумму отрицательных элеиентов массива
+
+// int GetNegativeSumm(int [] arr)
+// {
+//     int sum = 0;
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] < 0)
+//         {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// // 3. Получить сумму положительных элементов массива
+
+// int GetPositiveSumm(int [] arr)
+// {
+//     int sum = 0;
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] > 0)
+//         {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// Задача 32. Написать программу замены элементов массива: положительные элементы заменить на соответствующие отрицательные и наоборот
+
+// int [] array = GetArray(12, -9, +9);
+
+// Console.WriteLine(String.Join(" ,", array));
+// Console.WriteLine(String.Join(" ,", InvertArray(array)));
+
+// // 1. Метод, который возвращает массив, заполненный случайными числами из заданного промежутка
+
+// int [] GetArray(int count, int minValue, int maxValue)
+// {
+//     int [] array = new int [count];
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// // 1.1. Метод инверсии элементов
+
+// int [] InvertArray(int [] arr)
+// {
+//     for (int i  = 0; i < arr.Length; i++)
+//     {
+//         //arr[i] *= -1;
+//         arr[i] = arr[i] * (- 1); // или можно записать вот так
+//     }
+//     return arr;
+// }
+
+// // 2. Получить сумму отрицательных элеиентов массива
+
+// int GetNegativeSumm(int [] arr)
+// {
+//     int sum = 0;
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] < 0)
+//         {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// // 3. Получить сумму положительных элементов массива
+
+// int GetPositiveSumm(int [] arr)
+// {
+//     int sum = 0;
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] > 0)
+//         {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
+// Задача 33. Задать массив. Написать программу, которая определяет, присутствует ли заданное число в массиве. 
+
 int [] array = GetArray(12, -9, +9);
 
 Console.WriteLine(String.Join(" ,", array));
-Console.WriteLine($"Сумма положительных чисел {GetPositiveSumm(array)}");
-Console.WriteLine($"Сумма отрицательных чисел {GetNegativeSumm(array)}");
 
-// 1. Метод, который возвращает массив, заполненный случайными числами из заданного промежутка
+Console.WriteLine("Введите число: ");
+int k = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Число {k} найдено ? : {FindElement(k, array)}");
+
+// // 1. Метод, который возвращает массив, заполненный случайными числами из заданного промежутка
 
 int [] GetArray(int count, int minValue, int maxValue)
 {
@@ -94,34 +213,61 @@ int [] GetArray(int count, int minValue, int maxValue)
     return array;
 }
 
-// 2. Получить сумму отрицательных элеиентов массива
+// // 1.1. Функция, которая возвращает логическое значение (bool) "истина" или "ложь"
 
-int GetNegativeSumm(int [] arr)
+bool FindElement(int digit, int [] arr)
 {
-    int sum = 0;
-
-    for (int i = 0; i < arr.Length; i++)
+    for (int i  = 0; i < arr.Length; i++)
     {
-        if (arr[i] < 0)
-        {
-            sum += arr[i];
-        }
+        if (arr[i] == digit) return true;
     }
-    return sum;
+    return false;
 }
 
-// 3. Получить сумму положительных элементов массива
+// int [] InvertArray(int [] arr)
+// {
+//     for (int i  = 0; i < arr.Length; i++)
+//     {
+//         //arr[i] *= -1;
+//         arr[i] = arr[i] * (- 1); // или можно записать вот так
+//     }
+//     return arr;
+// }
 
-int GetPositiveSumm(int [] arr)
-{
-    int sum = 0;
+// // 2. Получить сумму отрицательных элеиентов массива
 
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] > 0)
-        {
-            sum += arr[i];
-        }
-    }
-    return sum;
-}
+// int GetNegativeSumm(int [] arr)
+// {
+//     int sum = 0;
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] < 0)
+//         {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// // 3. Получить сумму положительных элементов массива
+
+// int GetPositiveSumm(int [] arr)
+// {
+//     int sum = 0;
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] > 0)
+//         {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// Задача 35. Задать одномерный массив из 123 случайных чисел. Найти количество элементов массива, которые лежат в отрезке [10,99] 
+
+
+
+// Задача 37. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний элемент и т.д. Результат записать в новом массиве.
