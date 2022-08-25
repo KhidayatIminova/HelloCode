6ce98aa2d5bf2d49d055736c7c0412d2e9f8faad
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-25 22:56:16 +0700

    Add HW5_Task034

--- Seminar5/Homework5/Task034/Program.cs
+++ Seminar5/Homework5/Task034/Program.cs
@@ -3,3 +3,51 @@
 //[345, 897, 568, 234] -> 2
 
 Console.Clear();
+
+Console.Clear();
+
+int [] array = GetArray(4, 1, 999);
+
+Console.WriteLine($"[{string.Join(" ,",array)}]");
+Console.Write($"Количество четных чисел ");
+PrintEvenElements(array);
+Console.WriteLine($"в массиве = {GetCountEvenElements(array)}");
+
+// 1. Метод, который возвращает массив, заполненный случайными числами из заданного промежутка
+
+int [] GetArray(int count, int minValue, int maxValue)
+{
+    int [] array = new int [count];
+
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+// 2. Метод, который возвращает количество элементов с четными числами
+
+int GetCountEvenElements(int [] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {   
+        if (arr[i]%2 != 0) continue;
+        
+        else count++;
+    }
+    return count;
+}
+// 3. Метод вывода на экран четных элементов массива
+
+void PrintEvenElements(int [] arr)
+{
+        for (int i = 0; i < arr.Length; i++)
+    {   
+        if (arr[i]%2 != 0) continue;
+        //        Console.Write($"[{arr[i]}] ");
+        else Console.Write($" [{arr[i]}] ");
+    }
+}
