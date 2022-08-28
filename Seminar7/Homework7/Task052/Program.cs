8d405345fce47990c8d39b00f5c72cc96b9e3d6c
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-28 10:15:38 +0700

    Add HW7 Task052

--- Seminar7/Homework7/Task052/Program.cs
+++ Seminar7/Homework7/Task052/Program.cs
@@ -7,3 +7,86 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+Console.Clear();
+
+int m = 3;
+int n = 4;
+
+int [,] array = new int [3, 4];
+
+int a = 1;
+int b = 10;
+
+FillArray(array, a, b);
+PrintArray(array);
+Console.WriteLine();
+GetSumTable(array);
+Console.WriteLine();
+GetAverageTable(array);
+
+
+void FillArray(int [,] arr, int minValue, int maxValue)
+{
+   
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue);
+            
+        }
+    }
+}
+
+void PrintArray(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"  {arr[i,j]} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetAverageTable (int [,] arr)
+{
+    Console.WriteLine ($"\nСреднее арифметическое чисел столбцов массива:");
+    Console.WriteLine();
+
+    for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            double SumTable = 0;
+            double AverageTable = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                SumTable += arr[i,j];
+                AverageTable = SumTable/3;
+            }
+           
+            Console.Write ($"{(AverageTable):f2}  ");
+
+        }
+    
+}
+
+void GetSumTable (int [,] arr)
+{
+    Console.WriteLine ($"Сумма чисел столбцов массива:");
+    Console.WriteLine();
+
+    for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            double SumTable = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                SumTable += arr[i,j];
+                
+            }
+            
+            Console.Write ($" {SumTable}   ");
+
+        }
+    
+}
