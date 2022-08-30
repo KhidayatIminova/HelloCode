6e24070e4ca4c8fde2de533a263dba6b66e3775e
Khidayat_Iminova <hidayatka89@gmail.com>
2022-08-30 21:41:50 +0700

    Add HW6

---
+++ Seminar6/Homework6/Task041/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.Write("Сколько будет чисел? ");
+int M = int.Parse(Console.ReadLine());
+
+int [] array = GetArray(M);
+
+PrintArray(array);
+Console.WriteLine($"\nВсего введено чисел: {M} \nПоложительных чисел: {CountPos(array)}");
+Console.WriteLine();
+
+// 1. Метод заполнения массива с клавиатуры
+
+int [] GetArray(int size) 
+{
+    int [] array = new int [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1}-е число: "); 
+        array[i] = int.Parse(Console.ReadLine());
+        Console.Clear();
+    }
+    return array;
+}
+
+// 2. Метод вывода массива на экран
+
+void PrintArray(int [] arr)
+{
+    Console.Write("Введены числа: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+// 3. Метод, возвращающий значение счетчика положительных чисел
+
+int CountPos(int [] arr) 
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < 0 || arr[i] == 0) continue;
+            else count++;
+                    }
+        return count;
+}
+
---
+++ Seminar6/Homework6/Task043/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+Console.Write("Введите k1: ");
+int K1 = int.Parse(Console.ReadLine());
+Console.Write("Введите b1: ");
+int B1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+int K2 = int.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+int B2 = int.Parse(Console.ReadLine());
+
+Console.Clear();
+
+Console.WriteLine($"Уравнение прямой 1:   y = {K1} * x + {B1}");
+Console.WriteLine($"Уравнение прямой 2:   y = {K2} * x + {B2}");
+
+CheckCrossLine(K1, B1, K2, B2);
+Console.WriteLine();
+
+// 1. Метод проверки на параллельность с вызовом метода расчета точки пересечения
+
+void CheckCrossLine(double k1, double b1, double k2, double b2)
+{
+    if (k1 != k2) 
+    {
+        Console.Write("\nПрямые 1 и 2 пересекаются ");
+        GetCrossPoint(k1, b1, k2, b2);
+    }
+    else if (k1 == k2 && b1 != b2) Console.WriteLine("\nПрямые 1 и 2 параллельны");
+    else if (k1 == k2 && b1 == b2) Console.WriteLine("\nПрямые 1 и 2 совпадают");
+}
+
+// 2. Метод расчета точки пересечения
+
+void GetCrossPoint(double k1, double b1, double k2, double b2)
+{
+    double crossX = (b2 - b1)/(k1 - k2);
+    double crossY = (k1 * crossX + b1);
+    Console.WriteLine($"в точке с координатами ({crossX:f2};{crossY:f2})");
+}
